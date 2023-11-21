f6f87c631c4779cf4341660b92234d538e0f721a
MayaMazlom <mayamazloum16@gmail.com>
2023-11-21 20:58:37 +0200

    hotel reservation

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$( function() {
+    $( "#inputCheckIn" ).datepicker();
+    $( "#inputCheckOut" ).datepicker();
+
+    $('.level-slider').slick({
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+    })
+
+
+  } );
